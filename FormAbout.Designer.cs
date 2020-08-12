e259e9487f119d60ac53f0f95ec3088a1433c202
Madalin Rauch <madalin.rauch@gmail.com>
2020-08-12 05:29:11 +0300

    modified layout of FormAbout

--- FormAbout.Designer.cs
+++ FormAbout.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(361, 120);
             this.Controls.Add(this.labelAbout);
             this.Name = "FormAbout";
             this.Text = "FormAbout";
